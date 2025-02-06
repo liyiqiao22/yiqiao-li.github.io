13af3a1b3c09d06680a6c48737a78c7516bb6355
yiqiao-li <yli4@ccny.cuny.edu>
2025-02-06 15:02:21 +0000

    Deploying to gh-pages from @ yiqiao-li/yiqiao-li.github.io@06c684b34ea99fcbf2565f1556d76f9f09ed0ddb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,8 +51,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
+        },{id: "nav-group-members",
+          title: "Group Members",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
