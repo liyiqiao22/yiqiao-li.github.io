beb732b810603247e3fa7abd2e793030e2e4f362
yiqiao-li <yli4@ccny.cuny.edu>
2025-02-06 16:46:00 +0000

    Deploying to gh-pages from @ yiqiao-li/yiqiao-li.github.io@6a9231433a7d1e40d8efd0de95c3ea0c30dbf771 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "Projects",
+  },{id: "nav-research",
+          title: "Research",
           description: "",
           section: "Navigation",
           handler: () => {
