87c37f9b6c2556a233d41d492e66b564d4ed10b8
liyiqiao22 <yli4@ccny.cuny.edu>
2025-02-06 05:06:19 +0000

    Deploying to gh-pages from @ liyiqiao22/yiqiao-li.github.io@39415355595423690e09c68f8ab73f2288db3efe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-ai-amp-amp-mobility-research-lab-online",
+          title: 'AI &amp;amp;amp; Mobility Research Lab online',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
