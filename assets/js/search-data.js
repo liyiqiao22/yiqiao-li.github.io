5994be8f8caf18ca5ea35ee50d8302187d798368
yiqiao-li <yli4@ccny.cuny.edu>
2025-02-07 05:18:42 +0000

    Deploying to gh-pages from @ yiqiao-li/yiqiao-li.github.io@2bb63091b837d44ad2556dcf75f0aa0f3c16e67d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,9 +417,9 @@ ninja.data = [{
           description: "US DOT – UTC TBD National Center",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-1-congestion-pricing-effects-on-traffic-patterns-in-neighboring-underserved-communities",
+          title: '1.	Congestion Pricing Effects on Traffic Patterns in Neighboring Underserved Communities',
+          description: "US DOT – UTC Region 2 SEMPACT center",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
